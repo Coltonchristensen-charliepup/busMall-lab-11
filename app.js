36bfe4d27bac22d9668babd966c69c7df1d8e68b
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-11-04 21:49:39 -0800

    added content and a branch

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var imgTwoEl = document.getElementById('image-two'); // images -can give propert
 var imgThreeEl = document.getElementById('image-three'); // images -can give properties from the js
 var companyList = document.getElementById('companyList'); // render results
 console.log(imgOneEl);
+
+var ctx = document.getElementById('myChart');
+
+
 function Products(name) {
   this.name = name;
   this.src = `img/${name}.jpg`;
@@ -109,4 +113,44 @@ function handleSelections(event) {
     alert('click on the damn image');
   }
 }
+
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+
+
 myContentBin.addEventListener('click', handleSelections);
